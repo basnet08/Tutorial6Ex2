a339c9333bbda356430a53ba00a09f8185131638
basnet08 <110596748+basnet08@users.noreply.github.com>
2022-08-04 18:55:58 +1000

    Add project files.

---
+++ Tutorial6Ex2/Controllers/SubjectController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tutorial6Ex2.Controllers
+{
+    public class SubjectController : Controller
+    {
+        // GET: Subject
+        public string Index()
+        {
+            return "This page is to show subject homepage";
+        }
+        public string subjects()
+        {
+            return "this page is to show the list of subjects that the student is enrolled in";
+        }
+        public string Details(string SubjectID)
+        {
+            return HttpUtility.HtmlEncode("Subject code: " + SubjectID);
+
+        }
+        public ActionResult Edit(int SubjectID)
+        {
+            //update subject ti the database
+            return RedirectToAction("Index");
+         }
+        public ActionResult Delete(int SubjectID)
+        {
+            //delete student from the database whose id matches with specidied id
+            return RedirectToAction("Index");
+        }
+    }
+}
